9277f866155fa1e25488843ebd7cb3534160f192
sebastianzeledon <51758213+sebastianzeledon@users.noreply.github.com>
2020-07-31 21:28:09 -0400

    Book Details
    Re-Uploaded

--- Geek_Text/book_details/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from book_details.models import *
-
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(Review)
