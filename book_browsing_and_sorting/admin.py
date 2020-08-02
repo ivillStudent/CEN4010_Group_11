95821a3fef9544f882c715167904504e107e8040
ivillStudent <67809906+ivillStudent@users.noreply.github.com>
2020-08-02 18:57:46 -0400

    books populated

--- book_browsing_and_sorting/admin.py
+++ book_browsing_and_sorting/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import genre, copiesSold
 
 # Register your models here.
+
+admin.site.register (genre)
+admin.site.register (copiesSold)
--- book_browsing_and_sorting/models.py
+++ book_browsing_and_sorting/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from bookdetails.models import BookInfo
+
 
 # Create your models here.
+
+class genre (models.Model):
+    book = models.ForeignKey(
+        BookInfo, on_delete=models.CASCADE, related_name="browsingGenre", null=True)
+
+    def __str__(self):
+        return '{} -- {} '.format(self.book.bookName, self.book.genre)
+
+class copiesSold (models.Model):
+    book = models.ForeignKey(
+        BookInfo, on_delete=models.CASCADE, related_name="browsingCopiesSold", null=True)
+
+    def __str__(self):
+        return '{} -- {} '.format(self.book.bookName, self.book.copiesSold)
