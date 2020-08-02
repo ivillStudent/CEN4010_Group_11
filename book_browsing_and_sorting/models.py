0efbd0a2e3318c224af7968c574235b49b848a51
ivillStudent <67809906+ivillStudent@users.noreply.github.com>
2020-08-02 19:00:17 -0400

    update

--- book_browsing_and_sorting/models.py
+++ book_browsing_and_sorting/models.py
@@ -17,4 +17,4 @@ class copiesSold (models.Model):
         BookInfo, on_delete=models.CASCADE, related_name="browsingCopiesSold", null=True)
 
     def __str__(self):
-        return '{} -- {} '.format(self.book.bookName, self.book.copiesSold)
+        return '{} -- {} '.format(self.book.copiesSold, self.book.bookName)
