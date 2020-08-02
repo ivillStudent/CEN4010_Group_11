2ce696cf79b6085a0bf776f021509da7c0af8cc2
ipromiseshes18 <67117981+ipromiseshes18@users.noreply.github.com>
2020-08-02 13:44:13 -0400

    update

--- cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CartsView, Cart, CartItem, Order, OrderItem, ItemsSaved
-
-admin.site.register(CartsView)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(OrderItem)
-admin.site.register(Order)
-admin.site.register(ItemsSaved)
